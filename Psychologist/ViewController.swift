93b8c6091af010fa5c65df859c277921c9b2de82
Zoe Jin <zoe.jin@britny.se>
2015-08-18 08:43:16 +0200

    Initial Commit

---
+++ Psychologist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Zoe Jin on 18/08/15.
+//  Copyright (c) 2015 Zoe Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
